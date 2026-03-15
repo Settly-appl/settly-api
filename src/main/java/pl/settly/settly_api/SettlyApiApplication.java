10f05369233ba8db9ef961eed36cf1481ae6a76d
Mateusz Dutkiewicz <matexmatex95@gmail.com>
2026-03-15 21:01:41 +0100

    Initial commit

---
+++ src/main/java/pl/settly/settly_api/SettlyApiApplication.java
@@ -0,0 +1,13 @@
+package pl.settly.settly_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettlyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SettlyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/settly/settly_api/SettlyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.settly.settly_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettlyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
